fa436a0c226e681b53e98571d845fabdb025adea
Vishal Dangi <vishaldangi838@gmail.com>
2025-07-20 03:17:17 +0530

    week 5 handson completed

---
+++ src/Week5/account/src/main/java/com/cognizant/account/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.cognizant.account.controller;
+import com.cognizant.account.model.AccountDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+    @GetMapping("/accounts/{number}")
+    public AccountDetails getAccountDetails(@PathVariable String number) {
+        // Dummy response (no database)
+        return new AccountDetails(number, "savings", 234343);
+    }
+}
